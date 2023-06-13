9aa32285fe378cb0edb966649161c4db2b66fee7
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-13 20:42:51 +0900

    postモデルを更新

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :room_id, presence: true
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions 
   validates :rule_id, numericality: { other_than: 1, message: '対局ルールを選択してください' }
-  belongs_to: rule
+  belongs_to :rule
 end
