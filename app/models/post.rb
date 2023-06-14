5f573c048e7ea97e3a8a237b55ce61ee2849498a
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-14 21:48:39 +0900

    バリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  validates :room_id, presence: true
+
+  validates :room_number, presence: true, numericality: { message: "数字を入力してください"}, length: {minimum: 5}
+  validates :password, numericality: { message: "数字を入力してください"}, length: {minimum: 4}
 
   extend ActiveHash::Associations::ActiveRecordExtensions 
   validates :rule_id, numericality: { other_than: 1, message: '対局ルールを選択してください' }
