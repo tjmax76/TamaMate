8c1f3b51d7cab355bb0d8b99dc226d59ea597a99
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-14 21:49:02 +0900

    createアクションを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @posts = Post.all
+    if @post.name = ""
+      @post.name = "名無し"
+    end
+    if @post.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
   def post_params
+    params.require(:post).permit(:name, :rule_id, :room_number, :text, :password)
   end
 end
