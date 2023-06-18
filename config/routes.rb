71c1b7a6e4b6daaf982c3bae5c629b52869ab832
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-19 08:02:31 +0900

    comments#index, createのルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only:[:index, :create, :show, :destroy]
+  resources :posts, only:[:index, :create, :show, :destroy] do
+    resources :comments, only: [:index, :create]
+  end
 end
