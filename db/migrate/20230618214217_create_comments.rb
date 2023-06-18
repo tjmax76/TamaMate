4af66fdf1451564612de31f9cdbc9f453e970418
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-19 06:51:00 +0900

    コメントのマイグレーションファイルを作成

--- db/migrate/20230618214217_create_comments.rb
+++ db/migrate/20230618214217_create_comments.rb
@@ -1,7 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.string :name
+      t.text :text, null: false
+      t.integer :password
+      t.references :post, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_100656) do
+ActiveRecord::Schema.define(version: 2023_06_18_214217) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "text", null: false
+    t.integer "password"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_06_12_100656) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
