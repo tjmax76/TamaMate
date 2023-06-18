d4d2e26c11c97aa552766f1c17ec741b419681b2
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-19 06:56:23 +0900

    アソシエーションを記述

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  validates :text, presence: true
+  validates :password, numericality: true, allow_blank: true, length: {minimum: 4}
+
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :room_number, presence: true, numericality: true, length: {minimum: 5, maximum: 5}
   validates :password, numericality: true, allow_blank: true, length: {minimum: 4}
 
+  has_many :comments
   extend ActiveHash::Associations::ActiveRecordExtensions 
   validates :rule_id, numericality: { other_than: 1, message: 'を選択してください' }
   belongs_to :rule
