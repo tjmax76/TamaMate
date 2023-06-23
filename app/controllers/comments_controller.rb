c75bd4fa4ec73d573f4c19d1ca2c51b2f14346af
tjmax76 <113315287+tjmax76@users.noreply.github.com>
2023-06-23 20:01:39 +0900

    comments#createに処理を記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,5 +6,24 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @posts = Post.all.order(created_at: "DESC").page(params[:page]).per(5)
+    @comment = Comment.new(comment_params)
+    id = session[:id]
+    if id.to_i != 0
+      @rule = Rule.find(id)
+    end
+    set_name(@comment)
+
+    if @comment.save
+      succeeded_save(id)
+    else
+      faild_save(id)
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :text, :password).merge(post_id: params[:post_id])
   end
 end
